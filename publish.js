0e61d97f48e0c9aa91c2d5b95643dacfcc899bd5
kinkahuang <kinkahuang@tencent.com>
2016-06-22 19:32:27 +0800

    发布准备

---
+++ publish.js
@@ -0,0 +1,7 @@
+/**
+ * todo
+ * 1. webpack player.js & demo.js
+ * 2. copy img/css/libs demo.html exclude *.bat
+ * 3. 替换 css文件的图片路径为CDN路径 http://imgcache.qq.com/open/qcloud/video/vcplayer/img/xx.svg
+ * 4. 替换demo.html 引用路径
+ */
